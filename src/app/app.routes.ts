fc742e63d059e4bc2cd94d4d3cc002e239f1f84d
Rares Sabau <raressabau1284@gmail.com>
2023-11-23 16:54:10 +0200

    Added start of Ionic framework

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-
-export const routes: Routes = [{ path: 'login', component: LoginComponent },
-{ path: 'register', component: RegisterComponent },
-{ path: '', redirectTo: 'login', pathMatch: 'full' }];
