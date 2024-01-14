86b23148493164fba9762f77b2305db8fb5b0434
Rares Sabau <raressabau1284@gmail.com>
2024-01-14 16:27:45 +0200

    Taking Id for all parties

--- DataAccessLayer/obj/Debug/net6.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net6.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd993dde5ee7f43e70ecfa04eb45ca28ac9ecba1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd229eef3d5e757ad9fd787f3d3e04ee41c0e4ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- InstaPartyApp/obj/Debug/net6.0/InstaPartyApp.AssemblyInfo.cs
+++ InstaPartyApp/obj/Debug/net6.0/InstaPartyApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InstaPartyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd993dde5ee7f43e70ecfa04eb45ca28ac9ecba1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd229eef3d5e757ad9fd787f3d3e04ee41c0e4ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("InstaPartyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InstaPartyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
