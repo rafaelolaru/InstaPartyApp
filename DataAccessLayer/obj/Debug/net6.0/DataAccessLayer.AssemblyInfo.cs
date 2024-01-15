2bfc41cf5ee8d76c91cce80fed86d256267a4b83
Rares Sabau <raressabau1284@gmail.com>
2024-01-16 00:56:55 +0200

    Final design

--- DataAccessLayer/obj/Debug/net6.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net6.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e101fe2967af99c4ca214ecc8b486e3ca8ec95bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2713503086e2e9dd48c9a4018072e65e5d7e6c3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
